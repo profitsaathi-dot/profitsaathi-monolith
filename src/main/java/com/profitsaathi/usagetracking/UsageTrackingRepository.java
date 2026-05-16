096b6006856a8e4ef375d2cea6c821fda4a5866f
Aswinkrcssoft <128173810+Aswinkrcssoft@users.noreply.github.com>
2026-05-16 14:33:10 +0530

    enhanced

--- src/main/java/com/profitsaathi/usagetracking/UsageTrackingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.profitsaathi.usagetracking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UsageTrackingRepository extends JpaRepository<UsageTracking, Long> {
-    Optional<UsageTracking> findByCredentialsIdAndFeatureName(Long credentialsId, String featureName);
-}
